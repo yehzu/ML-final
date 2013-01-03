33160bf2cd632aa76f1dd3469c9fb027f0c79b98
yehzu <yehzu2@gmail.com>
2013-01-03 16:11:29 +0800

    replacing '?' to 'nan'

--- remove_missing_data.py
+++ remove_missing_data.py
@@ -1,8 +1,8 @@
 import sys
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
 	print "No enough arguments"
-	print "Usage: python remove_missing_data.py input_data output_data"
+	print "Usage:>>python remove_missing_data.py input_data output_data fill_value"
 
 i_file = open(sys.argv[1], 'r')
 o_file = open(sys.argv[2], 'w')
@@ -10,19 +10,16 @@ o_file = open(sys.argv[2], 'w')
 fileInput = i_file.readline()
 inputLine = fileInput.split()
 
-line = 0;
 
 while len(inputLine) > 0: 
-	miss = False
 	for i in range(1, len(inputLine)):
 		if inputLine[i] == '?':
-			miss = True
-			break
-	if not miss:
-		o_file.write(fileInput)
-		line = line + 1
-
+			inputLine[i] = sys.argv[3]
+	
+	for item in inputLine:
+		o_file.write('{0} '.format(item))
+	o_file.write('\n')
 	fileInput = i_file.readline()
 	inputLine = fileInput.split()
 
-print "There are {0} data".format(line)
+print "Finish!"
